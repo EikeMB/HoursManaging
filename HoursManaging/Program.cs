e965d7ec773b777c51b030ca20e4fb20feb7b568
eike <98343869+EikeMB@users.noreply.github.com>
2023-06-09 17:13:33 -0400

    added week class

--- HoursManaging/Program.cs
+++ HoursManaging/Program.cs
@@ -1,16 +1,38 @@
-﻿namespace HoursManaging
+﻿using System.Reflection.PortableExecutable;
+
+namespace HoursManaging
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            DateTime start = new DateTime(2023, 6, 7, 9, 15, 0);
-            DateTime end = new DateTime(2023, 6, 7, 21, 0, 0);
-            Day day = new Day(start, end);
+            DateTime startMonday = new DateTime(2023, 6, 5, 9, 15, 0);
+            DateTime endMonday = new DateTime(2023, 6, 5, 17, 15, 0);
+            Day monday = new Day(startMonday, endMonday);
+            DateTime startTuesday = new DateTime(2023, 6, 6, 9, 15, 0);
+            DateTime endTuesday = new DateTime(2023, 6, 6, 16, 15, 0);
+            Day tuesday = new Day(startTuesday, endTuesday);
+            DateTime startWed = new DateTime(2023, 6, 7, 9, 15, 0);
+            DateTime endWed = new DateTime(2023, 6, 7, 20, 30, 0);
+            Day wed = new Day(startWed, endWed);
+            DateTime startThur = new DateTime(2023, 6, 8, 9, 15, 0);
+            DateTime endThur = new DateTime(2023, 6, 8, 15, 30, 0);
+            Day thur = new Day(startThur, endThur);
 
-            Console.WriteLine(day.TotalHours);
-            Console.WriteLine(day.TotalMinutes);
-            Console.WriteLine(day.BreakTime);
+            Week weekJune5 = new Week(new DateTime(2023, 6, 5));
+
+            weekJune5.AddDay(monday);
+            weekJune5.AddDay(tuesday);
+            weekJune5.AddDay(wed);
+            weekJune5.AddDay(thur);
+
+            Console.WriteLine("start: " + weekJune5.StartDate);
+            Console.WriteLine("Hours: " + weekJune5.Hours);
+            Console.WriteLine("Minutes: " + weekJune5.Minutes);
+            Console.WriteLine("Missing hours: " + weekJune5.MissingHours);
+            Console.WriteLine("Missing minutes: " + weekJune5.MissingMinutes);
+            
+
         }
     }
 }
---
+++ HoursManaging/Week.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursManaging
+{
+    internal class Week
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        #region properties
+        public double WeeklyHours { get; set; }
+        public List<Day> Days { get; set; }
+
+        public double Hours
+        {
+            get
+            {
+                double totalHours = 0;
+                double minutes = 0;
+                foreach (Day day in Days)
+                {
+                    totalHours += day.TotalHours;
+                    minutes += day.TotalMinutes;
+                }
+                totalHours += Math.Floor(minutes / 60);
+                return totalHours;
+            }
+        }
+        public double Minutes
+        {
+            get
+            {
+                double totalMinutes = 0;
+                foreach (Day day in Days)
+                {
+                    totalMinutes += day.TotalMinutes;
+                }
+                totalMinutes = totalMinutes - (Math.Floor(totalMinutes / 60) * 60);
+                return totalMinutes;
+            }
+        }
+
+        public double MissingHours
+        {
+            get
+            {
+                if(WeeklyHours - Hours > 1)
+                {
+                    if(MissingMinutes > 0)
+                    {
+                        return WeeklyHours - Hours - 1;
+                    }
+                    return WeeklyHours - Hours;
+                }
+                return 0;
+                
+            }
+        }
+
+        public double MissingMinutes
+        {
+            get
+            {
+                if(WeeklyHours - Hours > 0)
+                {
+                    return 60 - Minutes;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        #endregion
+        public Week(DateTime startDate)
+        {
+            StartDate = startDate;
+            WeeklyHours = 35;
+            Days = new List<Day>(7);
+        }
+
+        public DateTime StartDate
+        {
+            get
+            {
+                return startDate;
+            }
+            set
+            {
+                if(value.DayOfWeek == DayOfWeek.Monday)
+                {
+                    startDate = value;
+                    endDate = startDate.AddDays(6);
+                }
+                else
+                {
+                    throw new ArgumentException("StartDate of the week must be a monday");
+                }
+            }
+        }
+
+        public void AddDay(Day day)
+        {
+            if(day.StartTime >= startDate && day.EndTime <= endDate)
+            {
+                if(Days.Count >= 7)
+                {
+                    throw new Exception("This week has already been filled");
+                }
+                int dayIndex = (int)(day.StartTime.DayOfWeek+6)%7;
+                Days.Insert(dayIndex, day);
+
+
+                
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Added day must be in the week you are trying to add it to.");
+            }
+
+        }
+
+
+    }
+}
