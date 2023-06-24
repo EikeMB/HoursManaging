1d9ab12ab666556545bdd0a050fb08a7dffdf1e7
eike <98343869+EikeMB@users.noreply.github.com>
2023-06-24 19:57:20 -0400

    Added daysbyweek

---
+++ HoursManaging/DaysByWeek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursManaging
+{
+    internal class DaysByWeek
+    {
+        public DateTime Week { get; set; }
+        public List<Day> Days { get; set; }
+
+        public double TotalHours { get; set; }
+        public double TotalMinutes { get; set; }
+
+    }
+}
